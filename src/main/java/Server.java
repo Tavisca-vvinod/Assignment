adfc50c54edcee909598890e832251e3ea899043
vvinod <vvinod@tavisca.in>
2019-08-01 12:01:35 +0530

    Main method and attributes of Server class. Next goal is to make handleClient method.

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,2 +1,24 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    static private String RESOURCE_PATH = "C:\\Users\\vvinod\\IdeaProjects\\AssignmentHttp\\src\\main\\resources";
+    static private String resourceToSend = "";
+    public static void main(String[] args)
+    {
+        try
+        {
+            ServerSocket server = new ServerSocket(8080);
+            while(true)
+            {
+                Socket client = server.accept();
+                new Thread(()->handleClient(client)).start();
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println("There has been an error "+e.getMessage());
+        }
+    }
 }
